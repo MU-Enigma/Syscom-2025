7e7708563a5f411ea9e1e4a3f0bf7313c2e628bb
Jaynth746 <jknsjayanth@gmail.com>
2025-10-09 15:21:10 +0530

    level1

---
+++ level1-bash/calculator/calculator.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Enter first number: "
+
+read num1
+
+echo "Enter second number: "
+
+read num2
+
+echo "Enter operation (+ - * /): "
+
+read op
+
+case $op in
+
++)
+    result=$((num1+num2))
+    ;;
+-)
+    result=$((num1-num2))
+    ;;
+\*)
+    result=$((num1*num2))
+    ;;
+/)
+    result=$((num1/num2))
+    ;;
+*)
+    echo "Invalid operator"
+    exit 1
+    ;;
+esac
+echo "Result: $result"
---
+++ level1-bash/variables/variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+name="Jayanth"
+
+echo "Hello, $name!"
